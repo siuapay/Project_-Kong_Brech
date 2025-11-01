ace08eb11f5ecfa98e14f55a9f65831dc5179799
siuapay <ctyonichan@gmail.com>
2025-11-01 09:51:38 +0700

    update new loai-sukien lan nua

--- src/main/java/com/branch/demo/controller/AdminControllerApiOfLoai.java
+++ src/main/java/com/branch/demo/controller/AdminControllerApiOfLoai.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/admin/api")
+@RequestMapping("/api")
 public class AdminControllerApiOfLoai {
 
     @Autowired
