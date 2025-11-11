c22c8dddcad1f096ee7270581f3e6aeea2a863ab
siuapay <ctyonichan@gmail.com>
2025-11-11 11:00:58 +0700

    update-test-success

--- src/main/resources/db/migration/V1__update_chap_su_chuc_vu_column.sql
+++
@@ -1,4 +0,0 @@
--- Migration to update chuc_vu column length in chap_su table
--- This fixes the "Data truncated" error when saving CHAP_SU enum value
-
-ALTER TABLE chap_su MODIFY COLUMN chuc_vu VARCHAR(50);
